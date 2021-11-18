a1e81d0c87f3b4d91e27eab2988f8ea35f7b0c80
Nishant Neeraj <nishant.neeraj@gmail.com>
2021-11-18 12:09:03 +0530

    ✅  100% code coverage in test

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -9,14 +9,51 @@ const minimalValidConf: Config = {
   region: "ap-south-1",
   tokenUse: "access",
 };
+// const validSub = "67915d9e-5d49-4e54-8970-6e331773c26c";
 
 const notExistingUserPool = "ap-south-1_oUjyqdXf2";
+const invalidIssuer = `https://cognito-idp.ap-south-1.amazonaws.com/${notExistingUserPool}`;
+
+const expiredToken =
+  "eyJraWQiOiI5XC9SdTRZelpsdjJCMVlxRE1KZlVyTmVrUWNPNE" +
+  "J0NkZ4aURkUlBOZlwvMUE9IiwiYWxnIjoiUlMyNTYifQ.eyJvcmlnaW5fanRpIjoiMzViMTM4" +
+  "ZWEtN2JhZi00NjE1LTg3YzMtOTk0MTdjZGYxNzQ3Iiwic3ViIjoiNjc5MTVkOWUtNWQ0OS00Z" +
+  "TU0LTg5NzAtNmUzMzE3NzNjMjZjIiwiZXZlbnRfaWQiOiI2ZGU4ODEzMi04ZGUwLTRhNTEtOD" +
+  "g4NS01YWNhNjBkODg3MjkiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2d" +
+  "uaXRvLnNpZ25pbi51c2VyLmFkbWluIiwiYXV0aF90aW1lIjoxNjM1MDU5MTQxLCJpc3MiOiJo" +
+  "dHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGgtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0a" +
+  "C0xX29VanlxZFhmMSIsImV4cCI6MTYzNzA3ODUzNSwiaWF0IjoxNjM3MDc0OTM1LCJqdGkiOi" +
+  "JmYjI4YTZiMi03OGFhLTRlMGUtOGQ1MC1iMzQ2ZDU2ZDQ2NjQiLCJjbGllbnRfaWQiOiIyMnA" +
+  "3MzVqZjdrcHFlYmRwbDA1dTJmcTZkdSIsInVzZXJuYW1lIjoiNjc5MTVkOWUtNWQ0OS00ZTU0L" +
+  "Tg5NzAtNmUzMzE3NzNjMjZjIn0.dZSXHcUnpTDs-K071c0kh7kZGOEgAAqtP-zjDvTJF-0gpnP" +
+  "SkQZybM7gMVtWq2Gj-Mtb4Xd3kFBewfBMj8WQ0ZV9S6rHPhuuAm9P73H-ozhtcqzigI7U815pn" +
+  "RYR1xvZ0P93AJF6YMlERJma2Qze34MGaf6PGE8-jUwEhgeyu0FqRTuqjpChaVA7C5soPIK198c" +
+  "cxMbT19qodJzBNtXCcs2m7AAmVZ7-0iaW-NIF75pRLPWWJGvVfT5dYqgMv0FVS6UgtnToh01vs" +
+  "K0kp9W-65U9MN0n6A3GSbgFJhSw39zphY7TI4_QGlMacf6lRTRb7M4GbK6P2LZsfSIdHFiKEg";
+
+const expiredIdToken =
+  "eyJraWQiOiJKWTlIY0doamJWK3FNK2s5aVVBYktOSDRxVG5ZUFNNcnd5QThOM1ZmR3FjPSIsImFsZyI" +
+  "6IlJTMjU2In0.eyJzdWIiOiI2NzkxNWQ5ZS01ZDQ5LTRlNTQtODk3MC02ZTMzMTc3M2MyNmMiLCJlbWF" +
+  "pbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmFwLXNvdXRoLTEuYW1" +
+  "hem9uYXdzLmNvbVwvYXAtc291dGgtMV9vVWp5cWRYZjEiLCJjb2duaXRvOnVzZXJuYW1lIjoiNjc5MTV" +
+  "kOWUtNWQ0OS00ZTU0LTg5NzAtNmUzMzE3NzNjMjZjIiwib3JpZ2luX2p0aSI6IjM1YjEzOGVhLTdiYWY" +
+  "tNDYxNS04N2MzLTk5NDE3Y2RmMTc0NyIsImF1ZCI6IjIycDczNWpmN2twcWViZHBsMDV1MmZxNmR1Iiw" +
+  "iZXZlbnRfaWQiOiI2ZGU4ODEzMi04ZGUwLTRhNTEtODg4NS01YWNhNjBkODg3MjkiLCJ0b2tlbl91c2U" +
+  "iOiJpZCIsImF1dGhfdGltZSI6MTYzNTA1OTE0MSwiZXhwIjoxNjM3MDc4NTM1LCJpYXQiOjE2MzcwNzQ" +
+  "5MzUsImp0aSI6Ijg2NGYyNWY3LWM1ZjgtNDYwNi04NjBjLTdmNGI2ZDM1YTU2NyIsImVtYWlsIjoibml" +
+  "zaGFudC5uZWVyYWorbG9jYWxAZ21haWwuY29tIn0.c72iCGF62GMYa57RscVotCHmvftagxSXy45DOHX" +
+  "Vy8dN6XFEe1-YJyY_u4gUsVavvm9y_bfBnABAdcWqRX7PtdPN15sNIs2sUa_w5Bd1cP3DS3E9PAthxcr" +
+  "VcxqlOZMA0ABRh96Q08ElthKuSspEVConDdB6DcJ33tzww5ulRM8n9YWCVvhz09p3zipuQOz-MVj1Gdy" +
+  "V9qTgG5Xldrtealh9vQ_Zhkf7NePANmmMceqrTk0ui2qEI1urE6lZ0GwrLGpCEtHTFIYPnTw23a68W2e" +
+  "86rbF3YDq1m_Jr7yjuqSNXysQwJDcLd1xAcmbdwZUOiLPk4c0LZbAt34B2CAQ7w";
+
+beforeEach(() => {
+  // Make sure each test starts fresh (can be set in jest.config.js with `restoreMocks: true`)
+  jest.restoreAllMocks();
+});
 
 describe("#init", () => {
   test("Initialization", async () => {
-    // jest.spyOn(ExpressAwsCognito.prototype, '').mockResolvedValueOnce();
-    // jest.spyOn(ExpressAwsCognito.prototype, 'loadIframe').mockReturnValueOnce();
-    // jest.spyOn(ExpressAwsCognito.prototype, 'constructor');
     console.error = jest.fn();
     const init = jest.spyOn(ExpressAwsCognito.prototype as any, "init");
     const instance = new ExpressAwsCognito({
@@ -111,13 +148,10 @@ describe("Testing Negative Scenarios", () => {
 
   test("Non existing user pool should call onInitFailed", function (done) {
     function onInitComplete() {
-      console.log("here!!!");
-      expect(true).toBe(false);
-      done();
+      done(new Error("Init complete called"));
     }
 
     function onInitFailed() {
-      console.log("failed:(");
       expect(true).toBe(true);
       done();
     }
@@ -143,8 +177,31 @@ describe("Testing Negative Scenarios", () => {
   test("Invalid token should throw error", function (done) {
     function onInitComplete() {
       try {
-        expect(middleware.validate("Some invalid token string")).toThrowError(
-          "The middleware is still initializing or failed during initialization."
+        expect(() =>
+          middleware.validate("Some invalid token string")
+        ).toThrowError(/Invalid JWT. Token provided:/);
+        done();
+      } catch (e) {
+        done(e);
+      }
+    }
+
+    function onInitFailed() {
+      done(new Error("Init failed called"));
+    }
+
+    const middleware = new ExpressAwsCognito({
+      ...minimalValidConf,
+      onInitComplete,
+      onInitFailed,
+    });
+  });
+
+  test("Expired token should throw error", function (done) {
+    function onInitComplete() {
+      try {
+        expect(() => middleware.validate(expiredToken)).toThrowError(
+          /jwt expired/
         );
         done();
       } catch (e) {
@@ -153,8 +210,86 @@ describe("Testing Negative Scenarios", () => {
     }
 
     function onInitFailed() {
-      expect(true).toBe(false);
-      done();
+      done(new Error("Init failed called"));
+    }
+
+    const middleware = new ExpressAwsCognito({
+      ...minimalValidConf,
+      onInitComplete,
+      onInitFailed,
+    });
+  });
+
+  test("Different token type should throw error", function (done) {
+    function onInitComplete() {
+      try {
+        expect(() => middleware.validate(expiredIdToken)).toThrowError(
+          /The configured user pool's token_use does not match with token's/
+        );
+        done();
+      } catch (e) {
+        done(e);
+      }
+    }
+
+    function onInitFailed() {
+      done(new Error("Init failed called"));
+    }
+
+    const middleware = new ExpressAwsCognito({
+      ...minimalValidConf,
+      onInitComplete,
+      onInitFailed,
+    });
+  });
+
+  test("Non matching key id should throw error", function (done) {
+    function onInitComplete() {
+      try {
+        expect(() => {
+          const fakeTokenObject = {
+            payload: { iss: middleware.issuer, token_use: "access" },
+            header: { kid: "Some garbage key id" },
+          };
+          (middleware as any).tokenSanityCheck(fakeTokenObject);
+        }).toThrowError(/Invalid access token. No matching key_id found/);
+        done();
+      } catch (e) {
+        done(e);
+      }
+    }
+
+    function onInitFailed() {
+      done(new Error("Init failed called"));
+    }
+
+    const middleware = new ExpressAwsCognito({
+      ...minimalValidConf,
+      onInitComplete,
+      onInitFailed,
+    });
+  });
+
+  test("Mismatch issues or issuer from a different pool should throw error", function (done) {
+    function onInitComplete() {
+      try {
+        expect(() => {
+          const fakeTokenObject = {
+            payload: { iss: invalidIssuer, token_use: "access" },
+            header: { kid: Object.keys(middleware.pems)[0] },
+          };
+          (middleware as any).tokenSanityCheck(fakeTokenObject);
+        }).toThrowError(
+          /The configured user pool does not match with token's user pool./
+        );
+        done();
+      } catch (e) {
+        done(e);
+      }
+    }
+
+    function onInitFailed() {
+      done(new Error("Init failed called"));
     }
 
     const middleware = new ExpressAwsCognito({
@@ -174,13 +309,50 @@ describe("Testing Positive Scenarios", () => {
 
   test("Valid config should call onInitComplete", function (done) {
     function onInitComplete() {
-      console.log("here!!!");
       expect(true).toBe(true);
       done();
     }
 
     function onInitFailed() {
-      console.log("failed:(");
+      done(new Error("Init failed called"));
+    }
+
+    new ExpressAwsCognito({
+      ...minimalValidConf,
+      onInitComplete,
+      onInitFailed,
+    });
+  });
+
+  // test("Valid token should return JSON payload", function (done) {
+  //   function onInitComplete() {
+  //     try {
+  //       const payload = middleware.validate(validToken);
+  //       expect(payload.sub).toEqual(validSub);
+  //       done();
+  //     } catch (e) {
+  //       done(e);
+  //     }
+  //   }
+
+  //   function onInitFailed() {
+  //     done(new Error("Init failed called"));
+  //   }
+
+  //   const middleware = new ExpressAwsCognito({
+  //     ...minimalValidConf,
+  //     onInitComplete,
+  //     onInitFailed,
+  //   });
+  // });
+
+  test("Config is sanitized and returne correctly", function (done) {
+    function onInitComplete() {
+      expect(true).toBe(true);
+      done();
+    }
+
+    function onInitFailed() {
       expect(true).toBe(false);
       done();
     }
@@ -191,22 +363,4 @@ describe("Testing Positive Scenarios", () => {
       onInitFailed,
     });
   });
-
-  // test("Config is sanitized and returne correctly", function (done) {
-  //   function onInitComplete() {
-  //     expect(true).toBe(true);
-  //     done();
-  //   }
-
-  //   function onInitFailed() {
-  //     expect(true).toBe(false);
-  //     done();
-  //   }
-
-  //   new ExpressAwsCognito({
-  //     ...minimalValidConf,
-  //     onInitComplete,
-  //     onInitFailed,
-  //   });
-  // });
 });
